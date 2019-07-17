a567e43fdfd15203146f61687d4185b829855bd3
shunya.kiyokawa@optim.co.jp <shunya.kiyokawa@optim.co.jp>
2019-07-17 16:29:56 +0900

    cookieについて補足

--- js/getCookie.js
+++ js/getCookie.js
@@ -3,7 +3,7 @@ cookievalue = $.cookie('pin1');
 
 // 削除されていたり、そもそも未登録の場合は、pin1にはundefinedが入っている
 if (cookievalue == undefined) {
-    $('h1').html("GetCookie.htmlを開き、cookieを設定してください");
+    $('h1').html("jQuery5.htmlを開き、cookieを設定してください。設定したら、このファイルをリロードしてください");
 } else {
     $('h1').html(cookievalue); //h1タグの文字の書き換え
 }
